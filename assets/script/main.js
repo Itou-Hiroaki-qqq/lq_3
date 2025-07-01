5070263248c47e3ca9d9e76dae8e79f0a7fd424b
Itou-Hiroaki-qqq <chiteijin315@gmail.com>
2025-07-02 08:27:46 +0900

    CSS(sp/pc)作成

---
+++ assets/script/main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    function initSlickSlider() {
+        if (window.innerWidth <= 767) {
+            if (!$('.slider').hasClass('slick-initialized')) {
+                $('.slider').slick({
+                    slidesToShow: 1,
+                    centerMode: true,
+                    centerPadding: '7.11%',
+                    arrows: false,
+                    dots: true,
+                    initialSlide: 1,
+                });
+            }
+        } else {
+            if ($('.slider').hasClass('slick-initialized')) {
+                $('.slider').slick('unslick');
+            }
+        }
+    }
+
+    $(document).ready(function () {
+        initSlickSlider();
+        $(window).on('resize', function () {
+            initSlickSlider();
+        });
+    });
+
+}); //script END
